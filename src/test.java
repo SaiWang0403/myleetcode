50e46cb8b7c66fed2d94a70d0164865d8735b274
sai <saiwangjava@gmail.com>
2023-12-20 19:35:28 -0800

    reset

--- src/test.java
+++ src/test.java
@@ -248,9 +248,7 @@ public class test {
 //        int c='4'-'0';
 //        System.out.println(c);
         System.out.println(5^2^0);
-        //this is tutu
-        // this is dahuang
-    }
+
 
 
 }
